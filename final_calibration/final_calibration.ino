9946f0c77e070badf9f6c5feaf67a7e5d7cf4e99
Iaroslav-Baskov <ymbaskov@gmail.com>
2026-03-04 17:41:13 +0200

    Update final_calibration.ino

--- final_calibration/final_calibration.ino
+++ final_calibration/final_calibration.ino
@@ -1,14 +1,10 @@
-#include "FS.h"
-#include "SD.h"
+
 #include "GY521.h"
 #include <SPI.h>
 #include <Wire.h>
 
 #define CS_PIN    4
-#define RST       14
-#define DIO0      27
 
-#define SD_CS     33
 #define SCK_PIN   18
 #define MISO_PIN  19
 #define MOSI_PIN  23
@@ -21,11 +17,14 @@
 GY521 sensor(0x68);
 
 const char* labels[]={
-"gx","gy","gz",
-"ax","ay","az",
-"gtemp",
-"magx","magy","magz",
+  "now[ms]",
+  "gx","gy","gz",
+  "ax","ay","az",
+  "gtemp",
+  "magx","magy","magz",
+  "volt",
 };
+
 
 struct float3d{
   float x,y,z;};
@@ -33,16 +32,31 @@ struct int16_t3d{
   int16_t x,y,z;};
   
 struct SensorData {
+  unsigned long now;
   float3d accel;
   float3d gyro;
   float gtemp;
   float3d mag;
 };
+struct device{
+  bool OK;
+  unsigned long last;
+  unsigned long last_alive;
+  unsigned long timeout;
+};
+struct SystemInfo {
+  device gyro,mag;
+};
+
 SensorData data;
+SystemInfo check;
 
-char row[512];
+char row[2048];
+int unitime;
+uint8_t payload[sizeof(SensorData) + 2];
 
-void getMag(float3d &output) {
+void getMag(float3d &output,float t) {
+    check.mag.last = millis();
     int16_t3d result;
   
     Wire.beginTransmission(QMC5883L_ADDRESS);
@@ -59,102 +73,120 @@ void getMag(float3d &output) {
       output.z=(float)result.z;
    }
 }
+void checkI2CDevices() {
+    check.gyro.OK = check.gyro.OK && i2cDevicePresent(0x68);
+//    Serial.print(check.gyro.OK);
+    check.mag.OK =check.mag.OK && i2cDevicePresent(QMC5883L_ADDRESS);
+//    Serial.println(check.mag.OK);
+}
+
 bool i2cDevicePresent(uint8_t address) {
     Wire.beginTransmission(address);
     return (Wire.endTransmission() == 0);
 }
-void sdConnect(){
-  Serial.println("sd connect");
-  if(SD.begin(SD_CS)){
-    if (!SD.exists("/final-calibration.csv")) {
-      File file = SD.open("/final-calibration.csv",FILE_WRITE);
-      if(file){
-        for(int i=0;i<sizeof(labels)/sizeof(labels[0]);i++){
-          file.print(labels[i]);
-          if(i<sizeof(labels)/sizeof(labels[0])-1)file.print(",");
-          else file.print("\n");
-        }
-        file.close();}
-    }
-  }
-  
-  Serial.println("sd connected");
-}
-void accelConnect(){
-  
-  Serial.println("accel connect");
-  while(!sensor.wakeup());
-    sensor.setAccelSensitivity(0);  //  2g
-    sensor.setGyroSensitivity(0);   //  250 degrees/s
-    sensor.setThrottle();
-  
-  
-}
-void magnConnect(){
-  Serial.println("magn connect");
-  Wire.beginTransmission(QMC5883L_ADDRESS);
-  Wire.write(0x09);
-  Wire.write(0b00011101); // OSR=512, 8G, 200Hz, continuous
-  Wire.endTransmission();
-
-  Wire.beginTransmission(QMC5883L_ADDRESS);
-  Wire.write(0x0B);
-  Wire.write(0x01);
-  Wire.endTransmission();
-}
-void startI2CDevices(){
-  delay(500);
-  Wire.begin(21,22);
-  Wire.setClock(100000); // Set to 100kHz (Standard Mode) for better stability
-  Wire.setTimeOut(50);   // Give it 50ms to recover before erroring out
-  delay(500);
-  accelConnect();
-  magnConnect();
+void dataToJson(SensorData data,char buffer[],int len){
+  int i=0;
+  snprintf(buffer, len,
+    "{%s:%d,%s:%.3f,%s:%.3f,%s:%.3f,%s:%.3f,%s:%.3f,%s:%.3f,%s:%.2f,%s:%.2f,%s:%.2f,%s:%.2f}\n",
+    labels[i++],data.now,
+    labels[i++],data.gyro.x,labels[i++],data.gyro.y,labels[i++],data.gyro.z,
+    labels[i++],data.accel.x,labels[i++],data.accel.y,labels[i++],data.accel.z,
+    labels[i++],data.gtemp,
+    labels[i++],data.mag.x,labels[i++],data.mag.y,labels[i++],data.mag.z);
 }
 void dataToCsv(SensorData data,char buffer[],int len){
   snprintf(buffer, len,
-    "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f\n",
+    "%d,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f\n",
+    data.now,
     data.gyro.x,data.gyro.y,data.gyro.z,
     data.accel.x,data.accel.y,data.accel.z,
     data.gtemp,
     data.mag.x,data.mag.y,data.mag.z
   );
 }
+void accelConnect(){
+//  Serial.println("accel connect");
+  check.gyro.last=millis();
+  if(i2cDevicePresent(0x68)){
+    check.gyro.OK=sensor.wakeup();
+    if(check.gyro.OK) {
+      sensor.setAccelSensitivity(0);  //  2g
+      sensor.setGyroSensitivity(0);   //  250 degrees/s
+      sensor.setThrottle();
+    }
+  }
+//  else Serial.println("accel not found");
+}
+void magnConnect(){
+//  Serial.println("magn connect");
+  check.mag.last=millis();
+  
+  check.mag.OK = i2cDevicePresent(QMC5883L_ADDRESS);
+  if(check.mag.OK){
+    Wire.beginTransmission(QMC5883L_ADDRESS);
+    Wire.write(0x09);
+    Wire.write(0b00011101); // OSR=512, 8G, 200Hz, continuous
+    Wire.endTransmission();
+  
+    Wire.beginTransmission(QMC5883L_ADDRESS);
+    Wire.write(0x0B);
+    Wire.write(0x01);
+    Wire.endTransmission();
+  }
+//  else Serial.println("magn no found");
+}
+void startI2CDevices(){
+  delay(500);
+  Wire.begin();
+  accelConnect();
+  magnConnect();
+}
 void setup() {
   Serial.begin(115200);
   SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN);
+  
+  startI2CDevices();
+  
   pinMode(HOT_PIN, OUTPUT);
   pinMode(CS_PIN, OUTPUT);
-  pinMode(SD_CS, OUTPUT);
+  
+  check.gyro.timeout=1000;
+  check.mag.timeout=1000;
+  
   digitalWrite(CS_PIN, HIGH);
-  digitalWrite(SD_CS, HIGH);
-  startI2CDevices();
-  sdConnect();
-  delay(100);
+  
 }
 
 void loop() {
-  sensor.read();
-    data.accel.x = sensor.getAccelX();
-    data.accel.y = sensor.getAccelY();
-    data.accel.z = sensor.getAccelZ();
   
-    data.gyro.x = sensor.getGyroX();
-    data.gyro.y = sensor.getGyroY();
-    data.gyro.z = sensor.getGyroZ();
-
-    data.gtemp = sensor.getTemperature();///340.0+36.53;
+  checkI2CDevices();
+  if(!(check.gyro.OK || check.mag.OK)){
+    startI2CDevices();
+  }
+  if(check.gyro.OK){
+      sensor.read();
+      data.accel.x = sensor.getAccelX();
+      data.accel.y = sensor.getAccelY();
+      data.accel.z = sensor.getAccelZ();
+    
+      data.gyro.x = sensor.getGyroX();
+      data.gyro.y = sensor.getGyroY();
+      data.gyro.z = sensor.getGyroZ();
+      data.gtemp =sensor.getTemperature();
+      check.gyro.last=millis();
+  }
+  else if (millis()-check.gyro.last>check.gyro.timeout){
+    accelConnect();
+  }
+  if(check.mag.OK){
+    getMag(data.mag,data.gtemp);}
+  else if (millis()-check.mag.last>check.mag.timeout){
+    magnConnect();
+  }
   
-  
-  getMag(data.mag);
-  digitalWrite(HOT_PIN,LOW);
-//  digitalWrite(HOT_PIN,millis()%1200000<600000);
+  data.now=millis();
   dataToCsv(data,row,sizeof(row));
   Serial.print(row);
-    File file = SD.open("/final-calibration.csv", FILE_APPEND);
-    if (file) {
-      file.print(row);
-      file.close();
-    }
-    delay(100);
+  delay(200);
+
 }
